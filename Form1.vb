6ec0e56a0de6a7b900b94ecdf842b9ee2c8c4119
Libor Knappek <libor.knappek@gmail.com>
2021-01-16 20:32:33 +0100

    first commit

---
+++ Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        'get Today
+        Dim ActualDate As Date = Now.Date
+        tbDate.Text = ActualDate.ToShortDateString
+
+
+        'Get last day before today from database
+        Dim query As String = "SELECT * FROM last_date"
+        Dim outputVal(-1, -1) As String
+
+        Dim response As DataTable = GetDBdata(query)
+        ReDim outputVal(response.Rows.Count - 1, response.Columns.Count - 1)
+        For j As Integer = 0 To response.Rows.Count - 1
+            For i As Integer = 0 To response.Columns.Count - 1
+                outputVal(j, i) = response.Rows(j).Item(i).ToString
+            Next
+        Next
+        Dim lastDBdate As Date = response.Rows(0).Item(0)
+
+        ' Get API Data
+        tbConsumption.Text = response.Rows(0).Item(0)
+        Dim targetDate As Date = lastDBdate.AddDays(-1)
+        Dim unixDate As Double = (targetDate.ToUniversalTime - Date.Parse("1970-01-01 00:00:00")).TotalSeconds
+        Dim APIresponse As String = GetAPIdata(unixDate)
+
+        'Deserialize json
+        Dim APIjson As JObject = JsonConvert.DeserializeObject(APIresponse)
+        Dim keyVal As JToken
+        Dim tokenValue As Decimal
+        For Each row In APIjson("data").ToList
+            keyVal = row("timestamp")
+            tokenValue = DirectCast(keyVal, JValue).Value
+            'DODĚLAT    ZDE
+        Next row
+        Dim test As String = APIjson("data")(0)("timestamp").ToString
+
+
+
+
+
+    End Sub
+End Class
---
+++ Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+    Public Function GetDBdata(query As String) As DataTable
+        Dim status As String
+        Dim connection As Odbc.OdbcConnection = New Odbc.OdbcConnection("DSN=HerokuDB")
+        connection.Open()
+        System.Console.WriteLine("State: " + connection.State.ToString())
+        status = connection.State.ToString()
+        Dim command As Odbc.OdbcCommand = New Odbc.OdbcCommand(query, connection)
+        Dim reader As Odbc.OdbcDataReader = command.ExecuteReader(CommandBehavior.CloseConnection)
+        Dim results As New DataTable
+        results.Load(reader)
+
+        reader.Close()
+        connection.Close()
+        Return results
+    End Function
+
+    Public Function GetAPIdata(unixTime As Double) As String
+        Dim endPoint As String = "https://api.visionq.cz/device_data.php?eui=0901288000016926&from=" & unixTime.ToString
+        Dim webClient As New Net.WebClient
+        Dim cc As System.Net.CredentialCache = New System.Net.CredentialCache
+        cc.Add(New Uri(endPoint), "Basic", New System.Net.NetworkCredential("libor.knappek@gmail.com", "Liborknappek1+"))
+        webClient.Credentials = cc
+        Dim result As String = webClient.DownloadString(endPoint)
+        Return result
+    End Function
+
+End Module
